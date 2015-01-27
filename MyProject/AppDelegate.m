5fe2038949af51ed2298ce31f6b4cbb55b75707d
matthew <an.thomas.an@gmail.com>
2015-01-27 22:57:12 +0800

    write a sampel code

--- MyProject/AppDelegate.m
+++ MyProject/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <HLUtils.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"hello %@",     [HLUtils hello]);
     return YES;
 }
 
